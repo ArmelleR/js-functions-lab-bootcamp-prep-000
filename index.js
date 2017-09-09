c31648e54163c167cc3c39e6cc1ed89565a42da7
Armelle Rose-Olesco <armelleroseolesco@gmail.com>
2017-09-09 01:35:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 // write your code below!
-function happyHolidays(string) {
+function happyHolidays {
   return string("Happy Holidays!")
 }
 
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 
-function happyHolidaysTo(holiday, name) {
-  return string(`Happy ${holiday}, ${name}!`)
-  HappyHolidaysTo('Thanksgiving, Jane')
-
-}
-
-function holidayCountdown(days, Thanksgiving) {
-  return string(`It's ${days} days until ${holiday}!`)
-}
-holidayCountdown parseInt(75)
